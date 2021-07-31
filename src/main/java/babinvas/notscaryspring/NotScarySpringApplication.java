bb12a815b3003001e9074882a9a13bb0b53dd3bf
babinvas <babinvas@gmail.com>
2021-07-31 19:13:15 +0300

    Step1: Starting a WEB server with Spring Boot

---
+++ src/main/java/babinvas/notscaryspring/NotScarySpringApplication.java
@@ -0,0 +1,12 @@
+package babinvas.notscaryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotScarySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotScarySpringApplication.class, args);
+	}
+}
